c8921b4c95f1a3b11f702bd45586abd1c8a69a26
Gaurav Kandalkar <gakandalkar@gmail.com>
2015-12-06 23:49:10 +0000

    first commit

---
+++ src/test/java/com/gaurav/service/CustomerDetailsTest.java
@@ -0,0 +1,19 @@
+package com.gaurav.service;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.Test;
+import org.mockito.Mock;
+
+
+public class CustomerDetailsTest {
+
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		
+	}
+
+}
